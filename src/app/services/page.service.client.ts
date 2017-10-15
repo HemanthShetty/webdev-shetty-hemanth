e107fc6ffa4bc3e891f89f07a0cc66da301ef83b
hemanth.shetty.m@gmail.com <hemanth.shetty.m@gmail.com>
2017-10-14 20:26:15 -0400

    Angular 2 services for Client
    Angular 2 services for Client

---
+++ src/app/services/page.service.client.ts
@@ -0,0 +1,62 @@
+import { Injectable } from '@angular/core';
+import { Http, RequestOptions, Response } from '@angular/http';
+import 'rxjs/Rx';
+import { environment } from '../../environments/environment';
+import { Router } from '@angular/router';
+
+// injecting service into module
+@Injectable()
+
+export class PageService {
+
+  pages = [
+              { '_id': '321', 'name': 'Post 1', 'websiteId': '456', 'description': 'Lorem' },
+              { '_id': '432', 'name': 'Post 2', 'websiteId': '456', 'description': 'Lorem' },
+              { '_id': '543', 'name': 'Post 3', 'websiteId': '456', 'description': 'Lorem' }
+          ];
+
+
+  createPage(websiteId, page) {
+    page._id = Math.random();
+    page.developerId = websiteId;
+    this.pages.push(page);
+    return page;
+  }
+
+  findPageByWebsiteId(websiteId) {
+    const result = [];
+    let index = 0;
+    for (let x = 0 ; x < this.pages.length; x++) {
+      if (this.pages[x].websiteId === websiteId) {
+        result[index] = this.pages[x];
+        index++;
+      }
+      return result;
+    }
+  }
+
+  findPageById(pageId: string) {
+    for (let x = 0; x < this.pages.length; x++) {
+      if (this.pages[x]._id === pageId) {  return this.pages[x]; }
+    }
+  }
+
+  updatePage(pageId, page) {
+    for (let x = 0; x < this.pages.length; x++) {
+      if (this.pages[x]._id === pageId) {
+        this.pages[x].name = page.firstName;
+        this.pages[x].description = page.password;
+        return this.pages[x];
+      }
+    }
+  }
+
+  deletePage(pageId) {
+    for (let x = 0; x < this.pages.length; x++) {
+      if (this.pages[x]._id === pageId) {
+        delete this.pages[x];
+      }
+    }
+  }
+
+}
---
+++ src/app/services/website.service.client.ts
@@ -0,0 +1,72 @@
+import { Injectable } from '@angular/core';
+import { Http, RequestOptions, Response } from '@angular/http';
+import 'rxjs/Rx';
+import { environment } from '../../environments/environment';
+import { Router } from '@angular/router';
+
+
+@Injectable()
+
+export class WebsiteService {
+
+  constructor() { }
+
+  websites = [
+    { '_id': '123', 'name': 'Facebook',    'developerId': '456', 'description': 'Lorem' },
+    { '_id': '234', 'name': 'Tweeter',     'developerId': '456', 'description': 'Lorem' },
+    { '_id': '456', 'name': 'Gizmodo',     'developerId': '456', 'description': 'Lorem' },
+    { '_id': '890', 'name': 'Go',          'developerId': '123', 'description': 'Lorem' },
+    { '_id': '567', 'name': 'Tic Tac Toe', 'developerId': '123', 'description': 'Lorem' },
+    { '_id': '678', 'name': 'Checkers',    'developerId': '123', 'description': 'Lorem' },
+    { '_id': '789', 'name': 'Chess',       'developerId': '234', 'description': 'Lorem' }
+  ];
+
+  createWebsite(userId, website) {
+    website._id = Math.random();
+    website.developerId = userId;
+    this.websites.push(website);
+    return website;
+  }
+
+
+  findWebsitesByUser(userId) {
+    const result = [];
+    let index = 0;
+    for (let x = 0 ; x < this.websites.length; x++) {
+      if (this.websites[x].developerId === userId) {
+        result[index] = this.websites[x];
+        index++;
+      }
+    return result;
+    }
+  }
+
+
+  findWebsiteById(websiteId: string) {
+    for (let x = 0; x < this.websites.length; x++) {
+      if (this.websites[x]._id === websiteId) {  return this.websites[x]; }
+    }
+  }
+
+  updateWebsite(websiteId, website) {
+    for (let x = 0; x < this.websites.length; x++) {
+      if (this.websites[x]._id === websiteId) {
+        this.websites[x].name = website.firstName;
+        this.websites[x].description = website.password;
+        return this.websites[x];
+      }
+    }
+  }
+
+  deleteWebsite(websiteId) {
+    for (let x = 0; x < this.websites.length; x++) {
+      if (this.websites[x]._id === websiteId) {
+        delete this.websites[x];
+      }
+    }
+  }
+
+
+
+}
+
