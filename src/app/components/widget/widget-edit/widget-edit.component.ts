9868cd090d517b22f6fd630f0d0421e14a96ca98
hemanth.shetty.m@gmail.com <hemanth.shetty.m@gmail.com>
2017-10-15 22:03:46 -0400

    Widget edit component
    Widget edit component

--- src/app/components/widget/widget-edit/widget-edit.component.ts
+++ src/app/components/widget/widget-edit/widget-edit.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-widget-edit',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WidgetEditComponent implements OnInit {
 
-  constructor() { }
+  userId: String;
+  websiteId: String;
+  widgetId: String;
+  pageId: String;
+  constructor(private activatedRoute: ActivatedRoute) {}
 
   ngOnInit() {
+    this.activatedRoute.params.subscribe(params => {
+      this.userId = params['uid'];
+      this.websiteId = params['wid'];
+      this.pageId = params['pid'];
+      this.widgetId = params['wgid'];
+    });
   }
-
 }
