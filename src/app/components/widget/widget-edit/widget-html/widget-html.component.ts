3484ca03a1e1a889df43dc56ae049bccba337435
hemanth.shetty.m@gmail.com <hemanth.shetty.m@gmail.com>
2017-10-15 23:16:30 -0400

    Image+ Youtube components
    Image+ Youtube components

--- src/app/components/widget/widget-edit/widget-html/widget-html.component.ts
+++ src/app/components/widget/widget-edit/widget-html/widget-html.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {WidgetService} from '../../../../services/widget.service.client';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-widget-html',
@@ -7,9 +9,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WidgetHtmlComponent implements OnInit {
 
-  constructor() { }
+  textHtml: String;
+  userId: String;
+  websiteId: String;
+  pageId: String;
+  widgetId: String;
+  widgetEdit: Boolean;
+  widget = {};
+
+  constructor(private widgetService: WidgetService,
+              private activatedRoutes: ActivatedRoute) {
+  }
 
   ngOnInit() {
+    this.activatedRoutes.params.subscribe(params => {
+      this.userId = params['uid'];
+      this.websiteId = params['wid'];
+      this.pageId = params['pid'];
+      this.textHtml = '<p> Home Page</p>';
+      this.widgetId = params['wgid'];
+      if (this.widgetId) {
+        this.widget = this.widgetService.findWidgetById(this.widgetId);
+        this.widgetEdit = true;
+        this.textHtml = this.widget['text'];
+      }
+    });
+  }
+
+  createWidget() {
+    this.widget['widgetType'] = 'HTML';
+    this.widget['text'] = this.textHtml;
+    this.widgetService.createWidget(this.pageId, this.widget);
+  }
+
+  updateWidget() {
+    this.widget['widgetType'] = 'HTML';
+    this.widget['text'] = this.textHtml;
+    this.widgetService.updateWidget(this.widgetId, this.widget);
+  }
+
+  deleteWidget() {
+    this.widgetService.deleteWidget(this.widgetId);
   }
 
 }
